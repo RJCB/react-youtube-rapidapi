53188522eae5785a56ae444fababdd23fd37d213
RJCB <37097058+RJCB@users.noreply.github.com>
2023-05-08 22:19:06 -0400

    install dependencies, setup components, api call, render video cards

---
+++ src/components/index.js
@@ -0,0 +1,20 @@
+//List of components. List all components here rather than cluttering the App.js
+export { default as Navbar } from './Navbar';
+export { default as Feed } from './Feed';
+export { default as ChannelDetail } from './ChannelDetail';
+export { default as SearchFeed } from './SearchFeed';
+export { default as VideoDetail } from './VideoDetail';
+export { default as SearchBar } from './SearchBar';
+export { default as Sidebar } from './Sidebar';
+export { default as Videos } from './Videos';
+export { default as VideoCard } from './VideoCard';
+export { default as ChannelCard } from './ChannelCard';
+
+
+
+
+// Material UI components
+
+// Typography: is used for instead of <p> and headings
+// Stack: stack items in a row/column
+// Paper: Simple Div with elevation, that looks like its floating 
